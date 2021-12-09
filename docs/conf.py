f288cd9662a40eda347e1223369413d481c52a4e
xinetzone <xinzone@outlook.com>
2021-12-09 21:44:20 +0800

    +zh_CN

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,8 @@ release = version_ns['__version__']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'zh_CN'
+gettext_compact = False  # optional.
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
